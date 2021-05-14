90e4f099bdeba60a82cec13ce038a41e9e573651
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-05-13 19:34:13 -0500

    add: footer.js framework

---
+++ footer.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    const footerchanger = [
+        `Hi!`,
+        `What are you doing down here?`,
+        `English 2 PAP is a fun class`
+    ]
+
+    const footerchanger = footerchanger[Math.floor(Math.random() * 3)]
+})
